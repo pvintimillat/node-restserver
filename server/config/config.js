16af33fd9955cdd4c583328b9652a19faad2e5db
Paul Vintimilla <pvintimilla@ups.edu.ec>
2019-05-12 22:15:12 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,22 @@
 // Puerto
 // ======================================================
 process.env.PORT = process.env.PORT || 3000;
+
+// ======================================================
+// Entorno
+// ======================================================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ======================================================
+// Database
+// ======================================================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = "mongodb+srv://Paul:ab12CD34@cafe-8rcv0.mongodb.net/test?retryWrites=true";
+}
+
+process.env.URLDB = urlDB;
