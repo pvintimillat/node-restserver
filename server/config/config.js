be4e055c1e3d40b4c1ef8b500035d20ebd0519ee
Paul Vintimilla <pvintimilla@ups.edu.ec>
2019-05-12 23:03:12 -0500

    Prueba atlas

--- server/config/config.js
+++ server/config/config.js
@@ -14,10 +14,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // ======================================================
 let urlDB;
 
-if (process.env.NODE_ENV === 'dev') {
-    urlDB = 'mongodb://localhost:27017/cafe';
-} else {
+//if (process.env.NODE_ENV === 'dev') {
+  //  urlDB = 'mongodb://localhost:27017/cafe';
+//} else {
     urlDB = "process.env.MONGO_URI";
-}
+//}
 
 process.env.URLDB = urlDB;
