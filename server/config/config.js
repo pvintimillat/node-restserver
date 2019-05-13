c1b4851911b444b770f0068cef35d68e8aa580c6
Paul Vintimilla <pvintimilla@ups.edu.ec>
2019-05-13 00:00:15 -0500

    Cambios curso

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 // ======================================================
 // Puerto
 // ======================================================
@@ -15,10 +14,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // ======================================================
 let urlDB;
 
-//if (process.env.NODE_ENV === 'dev') {
-  //  urlDB = 'mongodb://localhost:27017/cafe';
-//} else {
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
     urlDB = "process.env.MONGO_URI";
-//}
+}
 
 process.env.URLDB = urlDB;
